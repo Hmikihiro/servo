55a7f5b5ff2f8a8b1827051e95ab2cf16d038642
Hayashi Mikihiro <34ttrweoewiwe28@gmail.com>
2024-08-12 02:13:13 +0900

    replace only is_valid_floating_point_number_string
    Signed-off-by: Hayashi Mikihiro <34ttrweoewiwe28@gmail.com>

--- components/script/dom/bindings/str.rs
+++ components/script/dom/bindings/str.rs
@@ -9,6 +9,7 @@ use std::hash::{Hash, Hasher};
 use std::marker::PhantomData;
 use std::ops::{Deref, DerefMut};
 use std::str::FromStr;
+use std::sync::LazyLock;
 use std::{fmt, ops, str};
 
 use chrono::prelude::{Utc, Weekday};
@@ -430,10 +431,9 @@ impl DOMString {
 
     /// <https://html.spec.whatwg.org/multipage/#valid-floating-point-number>
     pub fn is_valid_floating_point_number_string(&self) -> bool {
-        lazy_static! {
-            static ref RE: Regex =
-                Regex::new(r"^-?(?:\d+\.\d+|\d+|\.\d+)(?:(e|E)(\+|\-)?\d+)?$").unwrap();
-        }
+        static RE: LazyLock<Regex> = LazyLock::new(|| {
+            Regex::new(r"^-?(?:\d+\.\d+|\d+|\.\d+)(?:(e|E)(\+|\-)?\d+)?$").unwrap()
+        });
         RE.is_match(&self.0) && self.parse_floating_point_number().is_some()
     }
 
